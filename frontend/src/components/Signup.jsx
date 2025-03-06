1bc162a8da81b7d0952e394c89efd9aae57a54f9
altomizawa <alyssontomizawa@hotmail.com>
2025-03-06 07:37:50 -0300

    feat: signup

---
+++ frontend/src/components/Signup.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+const Signup = () => {
+  return (
+    <div>
+      <h1>Multi-Factor Authentication</h1>
+      <form className='flex flex-col gap-2 justify-left mt-12' action={handleSubmit}>
+        <div className='flex flex-col gap-2 justify-left'> 
+          <label className='text-left font-bold' htmlFor='userId'>User ID:</label>
+          <input onChange={onChange} className='border-b-2' name='userId' type="text" placeholder="User ID" />
+        </div>
+        <div className='flex flex-col gap-2 justify-left'> 
+          <label className='text-left font-bold' htmlFor='password'>Password:</label>
+          <input onChange={onChange} className='border-b-2' name='password' type="text" placeholder="Password" />
+        </div>
+        <button className='mt-12' type="submit">Login</button>
+      </form>
+      <p className='mt-4'>Don't have an account? <a>Click here</a> to create one</p>
+    </div>
+  )
+}
+
+export default Signup
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,9 +2,16 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { BrowserRouter, Routes, Route } from 'react-router'
+import Signup from './components/Signup.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<App />} />
+        <Route path='/signup' element={<Signup />} />
+      </Routes>
+    </BrowserRouter>
   </StrictMode>,
 )
